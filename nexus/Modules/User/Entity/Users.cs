ae4ee17e0719b5839f143fe81aa8ea37a4bd9535
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-06-16 01:04:49 +0700

    add user

---
+++ nexus/Modules/User/Entity/Users.cs
@@ -0,0 +1,37 @@
+﻿using nexus.Config.Database;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace nexus.Modules.User.Entity
+{
+    public class Users : Timestamps
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+        
+        [Column("role_id")]
+        public Guid RoleId { get; set; }
+
+        [Column("image")]
+        public string Image { get; set; }
+
+        [Required]
+        [Column("fullname")]
+        public string Fullname { get; set; }
+
+        [Required]
+        [Column("username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column("password")]
+        public string Password { get; set; }
+
+        [Column("status")]
+        public string Status { get; set; }
+    }
+}
